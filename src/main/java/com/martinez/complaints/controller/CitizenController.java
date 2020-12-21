adf46ccad13665ed45ed69e238520152ba6b4c53
Antony Martinez <antonymartinez12@gmail.com>
2020-12-20 20:51:27 -0500

    Added save citizen

---
+++ src/main/java/com/martinez/complaints/controller/CitizenController.java
@@ -0,0 +1,27 @@
+package com.martinez.complaints.controller;
+
+import com.martinez.complaints.dto.CitizenDto;
+import com.martinez.complaints.service.CitizenService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class CitizenController {
+
+    private final CitizenService citizenService;
+
+    public CitizenController(CitizenService citizenService) {
+        this.citizenService = citizenService;
+    }
+
+    @PostMapping("/save")
+    public CitizenDto save(@RequestBody CitizenDto citizenDto){
+        return citizenService.save(citizenDto);
+    }
+
+}
