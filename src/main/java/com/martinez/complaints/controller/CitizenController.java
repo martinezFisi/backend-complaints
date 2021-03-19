bca7cbd3ac4938a3bc977938ee565b6cf70e566a
Antony Martinez <antony.martinez@seidor.com>
2021-03-18 22:12:26 -0500

    added restful best practices and added validations for /citizens

--- src/main/java/com/martinez/complaints/controller/CitizenController.java
+++ src/main/java/com/martinez/complaints/controller/CitizenController.java
@@ -3,14 +3,21 @@ package com.martinez.complaints.controller;
 import com.martinez.complaints.dto.CitizenDto;
 import com.martinez.complaints.service.CitizenService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.net.URI;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @Slf4j
 @RestController
-@RequestMapping("/citizen")
+@RequestMapping(value = "/citizens", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 public class CitizenController {
 
     private final CitizenService citizenService;
@@ -19,9 +26,12 @@ public class CitizenController {
         this.citizenService = citizenService;
     }
 
-    @PostMapping("/save")
-    public CitizenDto save(@RequestBody CitizenDto citizenDto){
-        return citizenService.save(citizenDto);
+    @PostMapping
+    public ResponseEntity<CitizenDto> save(HttpServletRequest request, @Valid @RequestBody CitizenDto citizenDto){
+        var citizen = citizenService.save(citizenDto);
+
+        var uri = URI.create(request.getRequestURI() + "/" + citizen.getId());
+        return ResponseEntity.created(uri).body(citizen);
     }
 
 }
