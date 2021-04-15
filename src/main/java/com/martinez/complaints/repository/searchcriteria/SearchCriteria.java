890e98e35d51f2cfa04a5abc75f8baf7f97a2d4d
Antony Martinez <antonymartinez12@gmail.com>
2021-04-14 20:30:19 -0500

    Added connectors AND(,) and OR(|) to /citizen?searchCriterias

--- src/main/java/com/martinez/complaints/repository/searchcriteria/SearchCriteria.java
+++ src/main/java/com/martinez/complaints/repository/searchcriteria/SearchCriteria.java
@@ -1,12 +1,17 @@
 package com.martinez.complaints.repository.searchcriteria;
 
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class SearchCriteria {
 
+    public final static String OR = "|";
+    public final static String AND = ",";
     private final String key;
     private final String operation;
     private final Object value;
+    private final String connector;
 
 }
