6fcddf98d09843b8b521105dcfa1b4ad31099263
Antony Martinez <antonymartinez12@gmail.com>
2021-05-30 20:11:30 -0500

    added DynamicPropertySource for testcontainers

---
+++ src/test/java/com/martinez/complaints/controller/CitizenControllerIntegrationTest.java
@@ -0,0 +1,38 @@
+package com.martinez.complaints.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+class CitizenControllerIntegrationTest {
+
+    @Container
+    public static final JdbcDatabaseContainer postgreSQLContainer = (JdbcDatabaseContainer) new PostgreSQLContainer("postgres:9.4")
+            .withInitScript("scripts/complaints-scripts-all-in-one.sql")
+            .withExposedPorts(5432);
+
+    @Autowired private MockMvc mvc;
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @Test
+    void test(){
+        System.out.println("test");
+    }
+}
