026d5a984ed6c9a3d051b8d3ec2f1722466ccb10
Antony Martinez <antonymartinez12@gmail.com>
2021-05-31 22:38:07 -0500

    Changed from JDK11 to JDK16, Added integration test for POST /api/v1/citizens

--- src/test/java/com/martinez/complaints/controller/CitizenControllerIntegrationTest.java
+++ src/test/java/com/martinez/complaints/controller/CitizenControllerIntegrationTest.java
@@ -1,7 +1,13 @@
 package com.martinez.complaints.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -11,6 +17,16 @@ import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Testcontainers
 @SpringBootTest
@@ -22,7 +38,9 @@ class CitizenControllerIT {
             .withInitScript("scripts/complaints-scripts-all-in-one.sql")
             .withExposedPorts(5432);
 
-    @Autowired private MockMvc mvc;
+    public static final String CITIZEN_BASE_URI = "/api/v1/citizens";
+
+    @Autowired private MockMvc mockMvc;
 
     @DynamicPropertySource
     static void postgresqlProperties(DynamicPropertyRegistry registry) {
@@ -31,8 +49,39 @@ class CitizenControllerIT {
         registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
     }
 
-    @Test
-    void test(){
-        System.out.println("test");
+    @DisplayName("""
+            Given a valid CitizenDto request \
+            When invoke a POST Method on URI "/api/v1/citizens" \
+            Then controller response a CitizenDto with Id generated
+            """)
+    @MethodSource("citizenDtoReqAndRes")
+    @ParameterizedTest
+    void testSuccessCreateCitizen(String reqCitizenDto, String resCitizenDto) throws Exception {
+        mockMvc.perform(post(CITIZEN_BASE_URI)
+                .contentType(APPLICATION_JSON)
+                .content(reqCitizenDto))
+               .andExpect(content().contentType(APPLICATION_JSON))
+               .andExpect(status().isCreated())
+               .andExpect(header().string("location", CITIZEN_BASE_URI.concat("/1")))
+               .andExpect(content().json(resCitizenDto));
+    }
+
+    private static Stream<Arguments> citizenDtoReqAndRes() throws JSONException {
+        var reqMap = Map.of("email", "citizen1990@gmail.com",
+                "password", "citizen1990",
+                "documentType", "L",
+                "documentNumber", "7777777",
+                "firstName", "Juan",
+                "lastName", "Garcia",
+                "age", 20);
+        var reqCitizenDto = new JSONObject(reqMap);
+
+        var resMap = new HashMap<>(reqMap);
+        resMap.put("id", 1);
+        var resCitizenDto = new JSONObject(resMap);
+
+        return Stream.of(
+                Arguments.of(reqCitizenDto.toString(), resCitizenDto.toString())
+        );
     }
 }
